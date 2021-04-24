18134cf911a1ab00096966618b9198ece3528bcc
dmitrav <dmitrav@inbox.ru>
2021-04-24 09:56:12 +0200

    v.0.0.16-cuda: umap for cell lines added

--- src/analysis.py
+++ src/analysis.py
@@ -5,9 +5,11 @@ from sklearn.preprocessing import StandardScaler
 from torchvision.io import read_image
 from src.models import Autoencoder
 from tqdm import tqdm
+from src.constants import cell_lines as all_cell_lines
+from src.constants import drugs as all_drugs
 
 
-def plot_umap_with_pars_and_labels(data, parameters, labels, save_to):
+def plot_umap_with_pars_and_labels(data, parameters, labels, save_to, umap_title=""):
 
     if not os.path.exists(save_to):
         os.makedirs(save_to)
@@ -28,10 +30,10 @@ def plot_umap_with_pars_and_labels(data, parameters, labels, save_to):
             seaborn.color_palette('colorblind')
             seaborn.axes_style('whitegrid')
             seaborn.scatterplot(x=embedding[:, 0], y=embedding[:, 1], hue=labels[hue], s=10)
-            pyplot.title('UMAP: n={}, metric={}'.format(neighbors, metric), fontsize=8)
-            pyplot.savefig(save_to + 'umap_hue={}_n={}_metric={}.png'.format(hue, neighbors, metric), dpi=300)
+            pyplot.title('{} UMAP: n={}, metric={}'.format(umap_title, neighbors, metric), fontsize=8)
+            pyplot.savefig(save_to + '{}_umap_hue={}_n={}_metric={}.png'.format(umap_title, hue, neighbors, metric), dpi=300)
             pyplot.close('all')
-            print('umap_hue={}_n={}_metric={}.png saved'.format(hue, neighbors, metric))
+            print('{}_umap_hue={}_n={}_metric={}.png saved'.format(umap_title, hue, neighbors, metric))
 
 
 def plot_full_data_umaps(path_to_drugs, save_path):
@@ -58,11 +60,36 @@ def plot_full_data_umaps(path_to_drugs, save_path):
     plot_umap_with_pars_and_labels(encodings, umap_pars, umap_labels, save_path)
 
 
+def plot_cell_lines_umaps(path_to_drugs, save_path):
+
+    for cell_line in tqdm(all_cell_lines):
+
+        image_paths = [f for f in os.listdir(path_to_drugs) if cell_line in f]
+        cell_lines = [f.split('_')[0] for f in image_paths]
+        plates = [f.split('_')[2] for f in image_paths]
+        wells = [f.split('_')[3] for f in image_paths]
+        drugs = [f.split('_')[4] for f in image_paths]
+        dates = ['_'.join(f.split('_')[-3:]) for f in image_paths]
+
+        encodings = []
+        for path in tqdm(image_paths):
+            img = read_image(path_to_drugs + path)
+            img_encoded = transform(img)
+            encodings.append(img_encoded.detach().cpu().numpy())
+        encodings = numpy.array(encodings)
+
+        umap_labels = {'drug': drugs}
+        umap_pars = [(10, 'euclidean'), (25, 'euclidean'), (50, 'euclidean'), (100, 'euclidean'), (500, 'euclidean'),
+                     (10, 'cosine'), (25, 'cosine'), (50, 'cosine'), (100, 'cosine'), (500, 'cosine'),
+                     (10, 'correlation'), (25, 'correlation'), (50, 'correlation'), (100, 'correlation'), (500, 'correlation')]
+
+        plot_umap_with_pars_and_labels(encodings, umap_pars, umap_labels, save_path, umap_title=cell_line)
+
+
 if __name__ == "__main__":
 
     path_to_drugs = 'D:\ETH\projects\morpho-learner\data\cut\\'
     path_to_ae_model = 'D:\ETH\projects\morpho-learner\\res\\aecl_0.6672_0.8192_e100\\ae.torch'
-    save_path = 'D:\ETH\projects\morpho-learner\\res\\aecl_0.6672_0.8192_e100\\clustering\\'
 
     device = torch.device('cuda')
 
@@ -73,4 +100,8 @@ if __name__ == "__main__":
 
     transform = lambda x: ae.encoder(torch.Tensor(numpy.expand_dims((x / 255.), axis=0)).to(device)).reshape(-1)
 
-    plot_full_data_umaps(path_to_drugs, save_path)
+    # save_path = 'D:\ETH\projects\morpho-learner\\res\\aecl_0.6672_0.8192_e100\\full_data_umaps\\'
+    # plot_full_data_umaps(path_to_drugs, save_path)
+
+    save_path = 'D:\ETH\projects\morpho-learner\\res\\aecl_0.6672_0.8192_e100\\cell_lines_umaps\\'
+    plot_cell_lines_umaps(path_to_drugs, save_path)
--- src/constants.py
+++ src/constants.py
@@ -2,6 +2,14 @@
 version = "v.0.0.16-cuda"
 user = 'andreidm'
 
+cell_lines = ['ACHN', 'HT29', 'M14',  # batch 1
+              'IGROV1', 'MDAMB231', 'SF539',   # batch 2
+              'HS578T', 'SKMEL2', 'SW620',  # batch 3
+              'EKVX', 'OVCAR4', 'UACC257',  # batch 4
+              'BT549', 'LOXIMVI', 'MALME3M',  # batch 5
+              'A498', 'COLO205', 'HOP62',  # batch 6
+              'HCT15', 'OVCAR5', 'T47D']  # batch 7
+
 drugs = ['Chlormethine', 'Clofarabine', 'Panzem-2-ME2', 'Pemetrexed', 'Asparaginase',
          'Irinotecan', 'Gemcitabine', '17-AAG', 'Docetaxel', 'Erlotinib',
          'UK5099', 'Fluorouracil', 'Everolimus', 'MEDICA 16', 'BPTES',
